28899612d556159f65a7691f2aa59e6ac4779035
Jiangwen Su <uukuguy@gmail.com>
2023-10-21 00:04:55 +0800

    Add speechless-reasoning-v0 dataset generate script

--- eval/run_multiple.sh
+++ eval/run_multiple.sh
@@ -7,11 +7,16 @@ PARENT_PATH=$(cd "${SCRIPT_PATH}/.." ; pwd)
 TEST_MODEL_PATH=$1
 TASK_NAME=$(basename ${TEST_MODEL_PATH})
 
+COMPLETION_LIMIT=$2
+if [ -z ${COMPLETION_LIMIT} ]; then
+	COMPLETION_LIMIT=1
+fi
+
 echo "Model Path: ${TEST_MODEL_PATH}"
 echo "Task Name: ${TASK_NAME}"
 
 # MULTIPL_E_RESULTS_DIR=eval_results/multipl_e/${TASK_NAME}
-MULTIPL_E_RESULTS_DIR=eval_results/multipl_e/${TASK_NAME}/multiple
+MULTIPL_E_RESULTS_DIR=eval_results/multipl_e/${TASK_NAME}/$(COMPLETION_LIMIT)-completions/multiple
 
 python ${SCRIPT_PATH}/multiple.py \
 		eval \
