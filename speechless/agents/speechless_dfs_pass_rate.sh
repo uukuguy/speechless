7ac3731d44bb2e6d4da6213f2c87a3d6f0ad2b38
Jiangwen Su <uukuguy@gmail.com>
2023-11-07 00:11:49 +0800

    Try ToolBench pass_rate evaluations

---
+++ speechless/agents/speechless_dfs_pass_rate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd tooleval 
+export TOOLBENCH_DATA_DIR=/opt/local/datasets/toolbench_data
+export CONVERTED_ANSWER_PATH=${TOOLBENCH_DATA_DIR}/reproduction_data/model_predictions_converted/ 
+export SAVE_PATH=pass_rate_results 
+export CANDIDATE_MODEL=speechless-tools-7b-32k-v0.15_dfs 
+python eval_pass_rate.py \
+    --converted_answer_path ${CONVERTED_ANSWER_PATH} \
+    --save_path ${SAVE_PATH} \
+    --reference_model ${CANDIDATE_MODEL} \
+    --test_ids ${TOOLBENCH_DATA_DIR}/test_query_ids/ \
+    --max_eval_threads 10 \
+    --evaluate_times 4
---
+++ speechless/agents/speechless_dfs_vs_chatgpt_cot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd tooleval
+export TOOLBENCH_DATA_DIR=/opt/local/datasets/toolbench_data
+export CONVERTED_ANSWER_PATH=${TOOLBENCH_DATA_DIR}/reproduction_data/model_predictions_converted/
+export SAVE_PATH=preference_results
+export PASS_TARE_PATH=pass_rate_results
+export REFERENCE_MODEL=chatgpt_cot
+export CANDIDATE_MODEL=speechless-tools-7b-32k-v0.15_dfs
+# export API_POOL_FILE=path/to/your/openai_key_json_file.json
+python eval_preference.py \
+    --converted_answer_path ${CONVERTED_ANSWER_PATH} \
+    --reference_model ${REFERENCE_MODEL} \
+    --output_model ${CANDIDATE_MODEL} \
+    --test_ids ../../data/test_ids/ \
+    --save_path ${SAVE_PATH} \
+    --pass_rate_result_path ${PASS_TARE_PATH} \
+    --max_eval_threads 10 \
+    --use_pass_rate true \
+    --evaluate_times 4
