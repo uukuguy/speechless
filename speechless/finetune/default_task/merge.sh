0ebe6eb3296cec4a096690c6e07abea70970306a
Jiangwen Su <uukuguy@gmail.com>
2024-02-07 01:05:40 +0800

    Fixed default_task/task.env

--- speechless/finetune/default_task/merge.sh
+++ speechless/finetune/default_task/merge.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # From speechless-code-mistral-7b-v1.0
-SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+SCRIPT_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 PARENT_PATH=$(cd "${SCRIPT_PATH}/.." ; pwd)
 
-source ${SCRIPT_PATH}/task.env
+source ${SCRIPT_ROOT}/task.env
 
 CHECKPOINT_DIR=${TASK_CHECKPOINT_DIR}/checkpoint-2122/adapter_model
 
