4320f6230e5f1b327971ef748245ef2c29315554
Jiangwen Su <uukuguy@gmail.com>
2024-05-16 01:45:38 +0800

    Updated unsloth

--- unsloth/mini_train.py
+++ unsloth/mini_train.py
@@ -1,21 +1,31 @@
+import os
 from unsloth import FastLanguageModel
 import torch
 from trl import SFTTrainer
 from transformers import TrainingArguments
 from datasets import load_dataset
-max_seq_length = 2048 # Supports RoPE Scaling interally, so choose any!
+import datasets
+
+seed = 18341
+max_seq_length = 2048  # Supports RoPE Scaling interally, so choose any!
+eval_size = 1000
 
 # Get LAION dataset
 # url = "https://huggingface.co/datasets/laion/OIG/resolve/main/unified_chip2.jsonl"
 # dataset = load_dataset("json", data_files = {"train" : url}, split = "train")
 
+model_root_dir = "/opt/local/llm_models/huggingface.co"
 train_file = "/opt/local/datasets/alpaca_gpt4/alpaca_gpt4_data.json"
-dataset = load_dataset("json", data_files=train_file, split="train")
+full_dataset = load_dataset("json", data_files=train_file, split="train")
+
+data_module = full_dataset.train_test_split(test_size=eval_size, shuffle=True, seed=seed)
+train_dataset = data_module['train']
+eval_dataset = data_module['test']
 
 # 4bit pre quantized models we support for 4x faster downloading + no OOMs.
 fourbit_models = [
     "unsloth/llama-3-8b-Instruct-bnb-4bit",
-    "unsloth/llama-3-8b-bnb-4bit", # [NEW] 15 Trillion token Llama-3
+    "unsloth/llama-3-8b-bnb-4bit",  # [NEW] 15 Trillion token Llama-3
     "unsloth/llama-3-70b-bnb-4bit",
     "unsloth/llama-3-70b-Instruct-bnb-4bit",
     "unsloth/mistral-7b-v0.2-bnb-4bit",
@@ -29,54 +39,67 @@ fourbit_models = [
     "unsloth/mistral-7b-bnb-4bit",
     "unsloth/llama-2-7b-bnb-4bit",
     "unsloth/gemma-7b-bnb-4bit",
-    "unsloth/gemma-7b-it-bnb-4bit", # Instruct version of Gemma 7b
+    "unsloth/gemma-7b-it-bnb-4bit",  # Instruct version of Gemma 7b
     "unsloth/gemma-2b-bnb-4bit",
-    "unsloth/gemma-2b-it-bnb-4bit", # Instruct version of Gemma 2b
+    "unsloth/gemma-2b-it-bnb-4bit",  # Instruct version of Gemma 2b
     "unsloth/Phi-3-mini-4k-instruct-bnb-4bit",
-] # More models at https://huggingface.co/unsloth
+]  # More models at https://huggingface.co/unsloth
 
 model, tokenizer = FastLanguageModel.from_pretrained(
     # model_name = "unsloth/llama-3-8b-bnb-4bit",
-    model_name = "unsloth/mistral-7b-v0.2-bnb-4bit",
-    max_seq_length = max_seq_length,
-    dtype = None,
-    load_in_4bit = True,
+    model_name=os.path.join(model_root_dir, "unsloth/mistral-7b-v0.2-bnb-4bit"),
+    max_seq_length=max_seq_length,
+    dtype=None,
+    load_in_4bit=True,
 )
 
 # Do model patching and add fast LoRA weights
 model = FastLanguageModel.get_peft_model(
     model,
-    r = 16,
-    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-                      "gate_proj", "up_proj", "down_proj",],
-    lora_alpha = 16,
-    lora_dropout = 0, # Supports any, but = 0 is optimized
-    bias = "none",    # Supports any, but = "none" is optimized
+    r=32,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ],
+    lora_alpha=64,
+    lora_dropout=0,  # Supports any, but = 0 is optimized
+    bias="none",  # Supports any, but = "none" is optimized
     # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
-    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
-    random_state = 3407,
-    max_seq_length = max_seq_length,
-    use_rslora = False,  # We support rank stabilized LoRA
-    loftq_config = None, # And LoftQ
+    use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+    random_state=3407,
+    max_seq_length=max_seq_length,
+    use_rslora=False,  # We support rank stabilized LoRA
+    loftq_config=None,  # And LoftQ
 )
 
 trainer = SFTTrainer(
-    model = model,
-    train_dataset = dataset,
-    dataset_text_field = "text",
-    max_seq_length = max_seq_length,
-    tokenizer = tokenizer,
-    args = TrainingArguments(
-        per_device_train_batch_size = 2,
-        gradient_accumulation_steps = 4,
-        warmup_steps = 10,
-        max_steps = 60,
-        fp16 = not torch.cuda.is_bf16_supported(),
-        bf16 = torch.cuda.is_bf16_supported(),
-        logging_steps = 1,
-        output_dir = "outputs",
-        optim = "adamw_8bit",
-        seed = 3407,
+    model=model,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    tokenizer=tokenizer,
+    args=TrainingArguments(
+        learning_rate=2e-4,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        warmup_steps=10,
+        # max_steps=60,
+        num_train_epochs=3,
+        fp16=not torch.cuda.is_bf16_supported(),
+        bf16=torch.cuda.is_bf16_supported(),
+        logging_steps=1,
+        save_strategy="steps",
+        save_steps=10,
+        eval_steps=10,
+        output_dir="outputs",
+        optim="adamw_8bit",
+        seed=3407,
     ),
 )
 trainer.train()
