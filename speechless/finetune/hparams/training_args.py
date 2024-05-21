9f11ebcc11a43ef357b5d5c8526d28d3f8ad964f
Jiangwen Su <uukuguy@gmail.com>
2024-05-21 10:54:27 +0800

    Fixed bugs

--- speechless/finetune/hparams/training_args.py
+++ speechless/finetune/hparams/training_args.py
@@ -6,5 +6,6 @@ class TrainingArguments(Seq2SeqTrainingArguments):
     num_early_stopping_train_epochs: int = field(default=0, metadata={"help": 'Number of training epochs before early stopping.'})
 
     def __post_init__(self):
+        super().__post_init__()
         if self.num_early_stopping_train_epochs <= 0:
             self.num_early_stopping_train_epochs = self.num_train_epochs
