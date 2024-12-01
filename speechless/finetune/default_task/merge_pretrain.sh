8c65e3a89a3c296fdd3866eff4a68404205b4261
Jiangwen Su <uukuguy@gmail.com>
2024-12-01 23:40:22 +0800

    Updated speechless.finetune.default_task/merge.sh

--- speechless/finetune/default_task/merge_pretrain.sh
+++ speechless/finetune/default_task/merge_pretrain.sh
@@ -5,14 +5,11 @@ PARENT_PATH=$(cd "${SCRIPT_PATH}/.." ; pwd)
 
 source ${SCRIPT_ROOT}/task.env
 
-# CKPT_STEPS=1392
-# CHECKPOINT_DIR=${TASK_CHECKPOINT_DIR}/checkpoint-${CKPT_STEPS}/adapter_model
-CHECKPOINT_DIR=${TASK_CHECKPOINT_DIR}/latest/adapter_model
+CKPT_STEPS=1392
 
-    # --merged_model_name_or_path ${TEST_MODEL_PATH}-${CKPT_STEPS}steps \
+CHECKPOINT_DIR=${TASK_CHECKPOINT_DIR}/pretrain/checkpoint-${CKPT_STEPS}
 
 python ${SCRIPT_ROOT}/merge_peft_adapters.py \
     --base_model_name_or_path ${BASE_MODEL_PATH} \
     --lora_model_path ${CHECKPOINT_DIR} \
-    --merged_model_name_or_path ${TEST_MODEL_PATH}
-
+    --merged_model_name_or_path ${TEST_MODEL_PATH}-${CKPT_STEPS}steps \
