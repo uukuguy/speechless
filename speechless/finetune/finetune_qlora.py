c7774805b972433149230c5002858fad3047e71b
Jiangwen Su <uukuguy@gmail.com>
2024-12-07 15:44:45 +0800

    Fixed speechless.finetune.finetune_qlora for dataset utils.

--- speechless/finetune/finetune_qlora.py
+++ speechless/finetune/finetune_qlora.py
@@ -32,7 +32,8 @@ Usage: python -m speechless.finetune.finetune_qlora \
     
 """
 import os, json
-from transformers import set_seed, Seq2SeqTrainer
+from transformers import set_seed, Seq2SeqTrainer, PreTrainedTokenizer
+from typing import Dict
 import bitsandbytes as bnb
 from loguru import logger
 
@@ -41,6 +42,152 @@ from speechless.utils.tokenizer_utils import get_tokenizer
 from speechless.finetune.dataset_utils import make_data_module
 from speechless.finetune.callbacks import LoggingCallback, CleanMemoryCallback, SavePeftModelCallback
 from speechless.finetune.finetune_qlora_arguments import get_args
+
+import pandas as pd
+from datasets import load_dataset, Dataset, concatenate_datasets
+from speechless.finetune.dataset_utils.data_collectors import DialogDataCollatorForCausalLM
+def make_data_module(tokenizer: PreTrainedTokenizer, args) -> Dict:
+
+    def local_dataset(dataset_name, test_size=0.02, stratify_by_column=None):
+        if "," in dataset_name:
+            all_datasets = []
+            for dataset_attr in [ n.strip() for n in dataset_name.split(',')]:
+                all_datasets.append(load_dataset("json", data_files=dataset_attr))
+            full_dataset = concatenate_datasets(all_datasets)
+        else:
+            if dataset_name.endswith(('.json', '.jsonl')):
+                full_dataset = Dataset.from_json(path_or_paths=dataset_name)
+            elif dataset_name.endswith('.csv'):
+                full_dataset = Dataset.from_pandas(pd.read_csv(dataset_name))
+            elif dataset_name.endswith('.tsv'):
+                full_dataset = Dataset.from_pandas(pd.read_csv(dataset_name, delimiter='\t'))
+            else:
+                raise ValueError(f"Unsupported dataset format: {dataset_name}")
+
+        if test_size < 1.0:
+            test_size = int(test_size * len(full_dataset))
+        else:
+            test_size = int(test_size)
+        if stratify_by_column is not None and stratify_by_column in full_dataset.column_names:
+            full_dataset = full_dataset.class_encode_column(stratify_by_column)
+
+        return full_dataset.train_test_split(test_size=test_size, stratify_by_column=stratify_by_column)
+
+
+    def load_data(dataset_name):
+        if os.path.exists(dataset_name):
+            try:
+                full_dataset = local_dataset(dataset_name, test_size=args.eval_dataset_size)
+                return full_dataset
+            except:
+                raise ValueError(f"Error loading dataset from {dataset_name}")
+        else:
+            raise NotImplementedError(f"Dataset {dataset_name} not implemented yet.")
+
+    def format_dataset(dataset, dataset_format):
+        assert dataset_format in ['instruction-response', 'conversations']
+
+        if dataset_format == 'conversations':
+
+            def _format_multi_turns(example):
+                human_bot_dialog = []
+                dialog = example['conversations']
+                for round in dialog:
+                    who = round['from']
+                    response = round['value']
+                    human_bot_dialog.append({
+                        "from": who,
+                        "value": response,
+                    })
+                return {
+                    'conversations': human_bot_dialog
+                }
+
+            dataset = dataset.map(_format_multi_turns)
+        elif dataset_format == 'instruction-response':
+            def _format_input_output(example):
+                return {
+                    'conversations': [
+                        {
+                            "from": "human", 
+                            "value": example['instruction'] 
+                        },
+                        {
+                            "from": "assistant", 
+                            "value": example['response'] 
+                        },
+                        ]
+                }
+
+            dataset = dataset.map(_format_input_output)
+
+        # Remove unused columns.
+        dataset = dataset.remove_columns(
+            # FIXME
+            [
+                col for col in dataset.column_names['train']
+                if col not in ['conversations', 'system_prompt', 'prompt_type']
+            ]
+        )
+        return dataset
+
+
+    # Load dataset.
+    dataset = load_data(args.dataset)
+
+    args.dataset_format = args.dataset_format if args.dataset_format else "instruction-response"
+    dataset = format_dataset(dataset, args.dataset_format)
+
+    logger.info(f"---------- Splitting dataset into train/eval ----------")
+    if args.do_eval or args.do_predict:
+        if 'eval' in dataset:
+            eval_dataset = dataset['eval']
+        elif 'test' in dataset:
+            eval_dataset = dataset['test']
+        else:
+            logger.info('Splitting train dataset in train and validation according to `eval_dataset_size`')
+            if 'category' in dataset["train"].column_names:
+                dataset["train"] = dataset["train"].class_encode_column('category')
+                dataset = dataset["train"].train_test_split(
+                    test_size=args.eval_dataset_size, stratify_by_column='category', seed=args.seed
+                )
+            else:
+                dataset = dataset["train"].train_test_split(
+                    test_size=args.eval_dataset_size, shuffle=True, seed=args.seed
+                )
+            eval_dataset = dataset['test']
+        if args.max_eval_samples is not None and len(eval_dataset) > args.max_eval_samples:
+            eval_dataset = eval_dataset.select(range(args.max_eval_samples))
+        if args.group_by_length:
+            eval_dataset = eval_dataset.map(lambda x: {'length': len(x['input']) + len(x['output'])})
+    if args.do_train:
+        train_dataset = dataset['train']
+        max_train_samples = args.max_train_samples
+        if max_train_samples is not None and max_train_samples > 0 and max_train_samples < 1.0:
+            max_train_samples = int(len(train_dataset) * max_train_samples)
+        else:
+            max_train_samples = 0.0
+        if max_train_samples >= 1.0 and len(train_dataset) > max_train_samples:
+            train_dataset = train_dataset.select(range(max_train_samples))
+
+        if args.group_by_length:
+            train_dataset = train_dataset.map(lambda x: {'length': len(x['input']) + len(x['output'])})
+
+    logger.info(f"{len(train_dataset)} training samples, {len(eval_dataset)} evaluation samples.")
+
+    data_collator = DialogDataCollatorForCausalLM(
+        tokenizer=tokenizer,
+        model_max_length=args.model_max_length,
+        prompt_type=args.prompt_type,
+    )
+
+    return dict(
+        train_dataset=train_dataset if args.do_train else None,
+        eval_dataset=eval_dataset if args.do_eval else None,
+        predict_dataset=eval_dataset if args.do_predict else None,
+        data_collator=data_collator
+    )
+
 
 def get_last_checkpoint(checkpoint_dir):
     if os.path.isdir(checkpoint_dir):
