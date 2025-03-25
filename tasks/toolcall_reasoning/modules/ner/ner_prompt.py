ba2e93b5e81977653d521726b2d6a1ffa025cf38
Jiangwen Su <uukuguy@gmail.com>
2025-03-25 12:48:10 +0800

    Added intent_prompt.py and ner_prompt.py in toolcall_reasoning/modules

---
+++ tasks/toolcall_reasoning/modules/ner/ner_prompt.py
@@ -0,0 +1,8 @@
+from textwrap import dedent
+
+system_content = dedent(""" You are a smart AI. Your task is to identify all sets of entity pairs that conform to the tool function definition specifications within the context of the user's reply content.
+    - The callable utility functions are detailed below, and the descriptions of the function names and parameter names accurately depict their functionality and scope. Please make careful judgments based on this context.
+    - Combine the tool functions and parameter descriptions to extract the most likely entities from the user's multi-turn dialogue content. The entity names should be represented by the parameters that best match their semantics, and the entity values should be directly extracted from the user content, with parameter types and descriptions obtained from the function definitions.
+    - Return the list of entities mapped to parameters in this format: "<function-name>function_name</function-name>\n<function-arguments>| argument_name | argument_value | argument_type | argument_description |\n</function_arguments>。<function-arguments></function-arguments>". 
+    - The XML tag <function-arguments></function-arguments> contains a list of corresponding parameter attributes separated by newline characters '\n'.
+""")
