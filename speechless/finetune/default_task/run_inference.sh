a12b80c4b28174f76a3e55baa6ab9a7533b6f6a7
Jiangwen Su <uukuguy@gmail.com>
2025-04-14 08:28:02 +0800

    Modified default_task/run_inference.sh

---
+++ speechless/finetune/default_task/run_inference.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+SCRIPT_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+PARENT_PATH=$(cd "${SCRIPT_PATH}/.." ; pwd)
+
+source ${SCRIPT_ROOT}/task.env
+
+TEST_DATA_PATH="test_data.jsonl"
+TEST_OUTPUT_PATH="inference_results.jsonl"
+
+python ${SCRIPT_ROOT}/inference.py \
+    --model_path ${TEST_MODEL_PATH} \
+    --test_path ${TEST_DATA_PATH} \
+    --output_path ${TEST_OUTPUT_PATH} \
+    --temperature 0.1 \
+    --max_tokens 2048 \
+
