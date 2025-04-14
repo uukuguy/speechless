62aad9b0ebeeaf9af436c2c7d1edccfd24bc67b4
Jiangwen Su <uukuguy@gmail.com>
2025-04-15 02:55:37 +0800

    Fixed logits_processor

--- speechless/losses/fixed_tag_loss.py
+++ speechless/losses/fixed_tag_loss.py
@@ -4,12 +4,19 @@ import torch
 from loguru import logger
 
 class FixedTagLoss(torch.nn.Module):
-    def __init__(self, tokenizer, fixed_tags: List):
+    def __init__(self, tokenizer, fixed_tags: List, fixed_tag_weight: float = 2.0, allowed_colors: List = None, allowed_token_weight: float = 1.0):
         super(FixedTagLoss, self).__init__()
         self.tokenizer = tokenizer
         assert isinstance(fixed_tags, list) and len(fixed_tags) > 0, "fixed_tags should be a non-empty list"
         self.fixed_tags = fixed_tags
         self.fixed_tags_ids = None
+        self.fixed_tag_weight = fixed_tag_weight
+        # allowed_colors = ('Black', 'Blue', 'Red', 'Green', 'Yellow', 'Gray', 'Pink', 'Orange', 'Purple', 'Brown')
+        self.allowed_colors = allowed_colors
+        self.allowed_token_weight = allowed_token_weight
+
+        self.allowed_token_ids = tokenizer.convert_tokens_to_ids(allowed_colors) if self.allowed_colors else []
+        self.allowed_token_ids = [torch.tensor(token_id).to(tokenizer.device) for token_id in self.allowed_token_ids]
 
     # def forward(self, model, inputs, return_outputs=False, num_items_in_batch=None):
     def forward(self, outputs, labels, num_items_in_batch=None):
@@ -20,12 +27,39 @@ class FixedTagLoss(torch.nn.Module):
         logits = outputs.logits
         weights = torch.ones_like(labels, dtype=torch.float)
         # weights = torch.tensor([1.0] * logits.size(-1), dtype=torch.float).to(labels.device)
-        for fixed_tag_id in self.fixed_tags_ids:
-            fixed_tag_len = len(fixed_tag_id)
+
+        fixed_tag_weight = self.fixed_tag_weight
+        allowed_token_weight = self.allowed_token_weight
+        # fixed_tag_weight = 2.0
+        # allowed_token_weight = 1.0
+
+        # fixed_tag_weight = 10.0
+        # allowed_token_weight = 2.0
+
+        def set_weights(tag_id, weight):
+            tag_len = len(tag_id)
             for i in range(labels.size(0)):
-                for j in range(labels.size(1) - fixed_tag_len + 1):
-                    if torch.equal(labels[i, j:j + fixed_tag_len], fixed_tag_id):
-                        weights[i, j:j + fixed_tag_len] = 2.0
+                for j in range(labels.size(1) - tag_len + 1):
+                    if torch.equal(labels[i, j:j + tag_len], tag_id):
+                        weights[i, j:j + tag_len] = weight
+
+        if fixed_tag_weight > 1.0:
+            for fixed_tag_id in self.fixed_tags_ids:
+                set_weights(fixed_tag_id, fixed_tag_weight)
+                # fixed_tag_len = len(fixed_tag_id)
+                # for i in range(labels.size(0)):
+                #     for j in range(labels.size(1) - fixed_tag_len + 1):
+                #         if torch.equal(labels[i, j:j + fixed_tag_len], fixed_tag_id):
+                #             weights[i, j:j + fixed_tag_len] = fixed_tag_weight
+
+        if self.allowed_token_ids and allowed_token_weight > 1.0:
+            for allowed_token_id in self.allowed_token_ids:
+                set_weights(allowed_token_id, allowed_token_weight)
+                # for i in range(labels.size(0)):
+                #     for j in range(labels.size(1)):
+                #         if labels[i, j] == allowed_token_id:
+                #             weights[i, j] = allowed_token_weight
+        
 
         # logger.debug(f"{logits.shape=}, {labels.shape=}, {weights.shape=}")
 
