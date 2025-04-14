2b85c65e98d482ebd5dad9348c88f68f16902d9c
Jiangwen Su <uukuguy@gmail.com>
2025-04-14 08:28:02 +0800

    Modified default_task/run_inference.sh

--- speechless/finetune/default_task/inference.py
+++ speechless/finetune/default_task/inference.py
@@ -16,49 +16,65 @@ model_path = "/opt/local/llm_models/huggingface.co/speechlessai/Finance2024-Qwen
 output_file = "Finance2024-Qwen2.5-7B-test-answers.jsonl"
 test_file = "/opt/local/datasets/competitions/IndustryCorpus2024/finance2024_test_data.jsonl"
 
-test_data = [json.loads(line.strip()) for line in open(test_file).readlines()]
-# raw_datas = [ json.loads(line.strip()) for line in open("/opt/local/datasets/competitions/CCKS2024/digital_finance_test_data.jsonl").readlines()]
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, help="Path to the model.")
+    parser.add_argument("--output_file", type=str, default="./inference_results.jsonl", help="Output file for generated answers.")
+    parser.add_argument("--test_file", type=str, help="Input test file.")
+    parser.add_argument("--max_tokens", type=int, default=1024, help="Maximum number of tokens to generate.")
+    parser.add_argument("--temperature", type=float, default=0.1, help="Temperature for sampling.")
+    return parser.parse_args()
 
-max_tokens = 2048
-# vllm
-model = VllmAIModel(model_path=model_path, max_tokens=max_tokens)
-sampling_params = {
-    'temperature': 0.1,
-    'max_tokens': max_tokens,
-    # 'use_beam_search': False,
-    # 'repetition_penalty': 1.0,
-    # 'length_penalty': 1.0,
-    # 'top_p': 1.0,
-    # 'top_k':  -1,
-    # 'seed': None,
-    # 'stop': = None,
-    # 'skip_special_tokens': True,
-}
+def main():
+    args = get_args()
+    model_path = args.model_path
+    test_file = args.test_file
+    output_file = args.output_file
+    temperature = args.temperature
+    max_tokens = args.temperature
 
-# # hf
-# gen_kwargs = {
-#     "temperature": 0.75, 
-#     "max_new_tokens": 2048,
-#     "do_sample": True,
-#     # "top_p": 0.9,
-#     "min_p": 0.1,
-# }
-# model = HFAIModel(model_path=model_path, max_tokens=max_tokens, bits=8, gen_kwargs=gen_kwargs)
+    test_data = [json.loads(line.strip()) for line in open(test_file).readlines()]
 
-max_examples = torch.cuda.device_count() * 4 
+    # vllm
+    model = VllmAIModel(model_path=model_path, max_tokens=max_tokens)
+    sampling_params = {
+        'temperature': temperature,
+        'max_tokens': max_tokens,
+        # 'use_beam_search': False,
+        # 'repetition_penalty': 1.0,
+        # 'length_penalty': 1.0,
+        # 'top_p': 1.0,
+        # 'top_k':  -1,
+        # 'seed': None,
+        # 'stop': = None,
+        # 'skip_special_tokens': True,
+    }
 
-prompts = [data['query'] for data in test_data]
-with open(output_file, 'w', encoding='utf-8') as fd:
-    for s, e, batch_responses in model.generate_batch(prompts, batch_size=max_examples, **sampling_params):
-    # for s, e, batch_responses in model.generate_batch(prompts, batch_size=max_examples):
-        assert e - s == len(batch_responses), f"{s=}, {e=}, {len(batch_responses)=}"
-        # for response, instruction, raw_data in zip(batch_responses, prompts[s:e], raw_datas[s:e]):
-        for response, instruction, raw_data in zip(batch_responses, prompts[s:e], test_data[s:e]):
-            logger.debug(f"{instruction=}")
-            logger.info(f"{response=}")
-            raw_data = deepcopy(raw_data)
-            raw_data['answer'] = response
-            fd.write(json.dumps(raw_data, ensure_ascii=False) + "\n")
-            fd.flush()
+    # # hf
+    # gen_kwargs = {
+    #     "temperature": temperature, 
+    #     "max_new_tokens": max_tokens,
+    #     "do_sample": True,
+    #     # "top_p": 0.9,
+    #     "min_p": 0.1,
+    # }
+    # model = HFAIModel(model_path=model_path, max_tokens=max_tokens, bits=8, gen_kwargs=gen_kwargs)
 
-print(f"Saved {len(test_data)} lines in {output_file}")
+    max_examples = torch.cuda.device_count() * 4 
+
+    prompts = [data['instruction'] for data in test_data]
+    with open(output_file, 'w', encoding='utf-8') as fd:
+        for s, e, batch_responses in model.generate_batch(prompts, batch_size=max_examples, **sampling_params):
+        # for s, e, batch_responses in model.generate_batch(prompts, batch_size=max_examples):
+            assert e - s == len(batch_responses), f"{s=}, {e=}, {len(batch_responses)=}"
+            # for response, instruction, raw_data in zip(batch_responses, prompts[s:e], raw_datas[s:e]):
+            for response, instruction, raw_data in zip(batch_responses, prompts[s:e], test_data[s:e]):
+                logger.debug(f"{instruction=}")
+                logger.info(f"{response=}")
+                raw_data = deepcopy(raw_data)
+                raw_data['response'] = response
+                fd.write(json.dumps(raw_data, ensure_ascii=False) + "\n")
+                fd.flush()
+
+    print(f"Saved {len(test_data)} lines in {output_file}")
