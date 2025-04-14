39c0ddc1ea82a820d7203528c7c1815aef2f3e44
Jiangwen Su <uukuguy@gmail.com>
2025-04-14 09:17:21 +0800

    Updated default_task.inference

--- speechless/finetune/default_task/run_inference.sh
+++ speechless/finetune/default_task/run_inference.sh
@@ -11,6 +11,8 @@ python ${SCRIPT_ROOT}/inference.py \
     --model_path ${TEST_MODEL_PATH} \
     --test_path ${TEST_DATA_PATH} \
     --output_path ${TEST_OUTPUT_PATH} \
+    --tensor_parallel_size ${TENSOR_PARALLEL_SIZE} \
+    --batch_size $((${TENSOR_PARALLEL_SIZE} * 4)) \
     --temperature 0.1 \
-    --max_tokens 2048 \
+    --max_tokens 2048 
 
