1b4ac3f1e6a53ff10460c74f8669b4b8195ec957
Jiangwen Su <uukuguy@gmail.com>
2025-04-14 09:19:52 +0800

    Updated default_task.inference

--- speechless/finetune/default_task/run_inference.sh
+++ speechless/finetune/default_task/run_inference.sh
@@ -4,13 +4,13 @@ PARENT_PATH=$(cd "${SCRIPT_PATH}/.." ; pwd)
 
 source ${SCRIPT_ROOT}/task.env
 
-TEST_DATA_PATH="test_data.jsonl"
-TEST_OUTPUT_PATH="inference_results.jsonl"
+TEST_FILE="test_data.jsonl"
+OUTPUT_FILE="inference_results.jsonl"
 
 python ${SCRIPT_ROOT}/inference.py \
     --model_path ${TEST_MODEL_PATH} \
-    --test_path ${TEST_DATA_PATH} \
-    --output_path ${TEST_OUTPUT_PATH} \
+    --test_file ${TEST_FILE} \
+    --output_file ${OUTPUT_FILE} \
     --tensor_parallel_size ${TENSOR_PARALLEL_SIZE} \
     --batch_size $((${TENSOR_PARALLEL_SIZE} * 4)) \
     --temperature 0.1 \
