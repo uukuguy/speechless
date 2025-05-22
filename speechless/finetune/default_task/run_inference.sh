c81cc9936e421f2250b7f8fc2a79e3ad848011ac
Jiangwen Su <uukuguy@gmail.com>
2025-05-22 17:07:23 +0800

    Updated run_inference.sh

--- speechless/finetune/default_task/run_inference.sh
+++ speechless/finetune/default_task/run_inference.sh
@@ -5,7 +5,8 @@ PARENT_PATH=$(cd "${SCRIPT_PATH}/.." ; pwd)
 source ${SCRIPT_ROOT}/task.env
 
 TEST_FILE="test_data.jsonl"
-OUTPUT_FILE="inference_results.jsonl"
+# replace ".jsonl" with "-inferred.jsonl"
+OUTPUT_FILE="${TEST_FILE%.jsonl}-inferred.jsonl"
 LOGITS_PROCESSOR_MODULE_FILE="${PWD}/logits_processor.py"
 LOGITS_PROCESSOR_CLASS_NAME=BacktickConstraint
 
@@ -13,6 +14,7 @@ PYTHONPATH=${SPEECHLESS_ROOT:-${HOME}/sandbox/LLM/speechless.ai/speechless} \
 python -m speechless.finetune.default_task.inference_openai \
         --model_name ${TEST_MODEL_PATH} \
         --test_file ${TEST_FILE} \
+        --output_file ${OUTPUT_FILE} \
         --base_url http://localhost:${VLLM_PORT}/v1 \
         --parallel_processes 4 \
         --parallel_chunk_size 16 \
