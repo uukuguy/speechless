ff89360f4571a9b09892867a475dbc16dcbcdda2
Jiangwen Su <uukuguy@gmail.com>
2025-06-12 08:17:38 +0800

    Update speechless.coding.scripts

--- coding/scripts/merge_sft.sh
+++ coding/scripts/merge_sft.sh
@@ -1,14 +1,22 @@
+#!/usr/bin/env bash
 # From speechless-code-mistral-7b-v1.0
 SCRIPT_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 PARENT_PATH=$(cd "${SCRIPT_PATH}/.." ; pwd)
 
-BASE_MODEL_PATH=Qwen3-4B
-CHECKPOINT_DIR=adapter_model
-TEST_MODEL_PATH=model_weights
+source ${SCRIPT_ROOT}/task.env
 
+# CKPT_STEPS=1392
+# CHECKPOINT_DIR=${TASK_CHECKPOINT_DIR}/checkpoint-${CKPT_STEPS}/adapter_model
+CHECKPOINT_DIR=${TASK_CHECKPOINT_DIR}/latest/adapter_model
 
+    # --merged_model_name_or_path ${TEST_MODEL_PATH}-${CKPT_STEPS}steps \
+
+# python ${SCRIPT_ROOT}/merge_peft_adapters.py \
+
+PYTHONPATH=${SPEECHLESS_ROOT} \
 python -m speechless.finetune.default_task.merge_peft_adapters \
     --base_model_name_or_path ${BASE_MODEL_PATH} \
     --lora_model_path ${CHECKPOINT_DIR} \
     --merged_model_name_or_path ${TEST_MODEL_PATH} \
     ${ADD_REASONING_TOKENS}
+
