433d1ff193f9bc941f6fbfbc784034ce3f57e0a1
Jiangwen Su <uukuguy@gmail.com>
2025-07-18 16:23:04 +0800

    Clear Update

--- tasks/toolcall_reasoning/modules/intent/intent_prompt.py
+++
@@ -1,9 +0,0 @@
-from textwrap import dedent
-
-system_content = dedent("""You are a smart AI. Your task is to accurately identify the list of tool function names that are most likely to be invoked in context.
-    - There can be a maximum of 4 correctly called utility functions, and a minimum of 0.
-    - The callable utility functions are detailed below, and the descriptions of the function names accurately depict their functionality and scope. Please make careful judgments based on this context.
-    - In the same context, the same function may be called multiple times, different functions may be called separately, or a combination of both situations may occur.
-    - The context provides the user's sequential multi-turn dialogue content, and we need to determine whether it is the right time to call the tool function in the last turn. Although most of the previous turns contain information about tool calls, the last turn may include content completely unrelated to tool calls. In this case, if the user has changed topics, no tool functions should be called, and an empty list should be returned instead.
-    - Return the list of identified function names in an <function-names></function-names> XML tag.
-""")
